75ccd361fe4265063e6c31ea172625e8a50e42e4
abhi <abhi@gmail.com>
2026-08-09 09:21:00 +0530

    added server-script.sh file

---
+++ server-script.sh
@@ -0,0 +1,20 @@
+sudo yum install java -y
+sudo yum install git -y
+sudo yum install maven -y
+#sudo yum install docker -y
+#sudo service docker start
+
+
+if [ -d "new-taxi-app" ]
+then
+  echo "repo is cloned and exists"
+  cd /home/ec2-user/new-taxi-app
+  git pull origin master
+else
+  git clone https://github.com/AbhishekK612/new-taxi-app.git
+fi
+
+cd /home/ec2-user/new-taxi-app
+mvn package
+
+#sudo docker build -t $1 .
